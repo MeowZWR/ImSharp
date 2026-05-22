69e02ee42c8802efa8dd9a575b9afa5593b05232
Exter-N <exter-n@exter-n.fr>
2026-05-22 19:10:40 +0200

    Remove the DirectX-specific subsystem

--- ImSharp/Resources/FsQuad_vs.hlsl
+++
@@ -1,9 +0,0 @@
-#include "include/FsQuad.hlsli"
-
-fs_quad_vertex main(uint id : SV_VertexID)
-{
-    fs_quad_vertex vertex;
-    vertex.uv       = float2(id & 1, (id & 2) >> 1);
-    vertex.position = float4(vertex.uv * float2(2.0, -2.0) + float2(-1.0, 1.0), 0.0, 1.0);
-    return vertex;
-}
